4cc0ee601d943c054a9744eeef50aec04146e0c6
Muhammad Nafees <nf10018@gmail.com>
2024-05-21 16:21:17 +0500

    mongodb connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-dotenv.config();
 import express from "express";
 import dotenv from "dotenv";
+import connectionDB from "./db/index.js";
 
+dotenv.config({path:".env"});
 const app = express();
 const PORT = process.env.PORT || 3000;
+connectionDB();
 
-
-app.get("/", (req, res) => {
-  res.send("Hey Home");
-});
-
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`https://localhost:${PORT}`);
 });
